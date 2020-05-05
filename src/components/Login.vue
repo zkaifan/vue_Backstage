d8e1f5612703328103d4b60ce0ea1c44422c2e7f
kaifan <860708868@qq.com>
2020-05-05 21:28:09 +0800

    add to login

---
+++ src/components/Login.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <h1>Login</h1>
+      <el-form
+        class="login_form"
+        ref="loginFormRef"
+        :model="loginForm"
+        :rules="loginFormRules"
+      >
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input
+            prefix-icon="iconfont icon-user"
+            v-model="loginForm.username"
+          ></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input
+            prefix-icon="iconfont icon-3702mima"
+            v-model="loginForm.password"
+            type="password"
+          ></el-input>
+        </el-form-item>
+        <!-- 按钮 -->
+        <el-form-item class="button">
+          <el-button type="primary" @click="Login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 登录表单数据绑定
+      loginForm: {
+        username: "admin",
+        password: "123456"
+      },
+      // 表单验证规则
+      loginFormRules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 5, max: 10, message: "长度在 5 到 10 个字符", trigger: "blur" }
+        ],
+        password: [{ required: true, message: "请输入密码", trigger: "blur" }]
+      }
+    };
+  },
+  methods: {
+    resetLoginForm() {
+      this.$refs.loginFormRef.resetFields();
+      this.loginForm.username = "";
+      this.loginForm.password = "";
+    },
+    Login() {
+      // 登录预验证
+      this.$refs.loginFormRef.validate(async valid => {
+        if (!valid) return;
+        const { data: res } = await this.$http.post("login", this.loginForm);
+        if (res.meta.status !== 200)
+          return this.$message.error("用户名或密码错误");
+        console.log("登录成功");
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.login_container {
+  width: 100vw;
+  height: 100vh;
+  background: #2b4b6b;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .login_box {
+    width: 350px;
+    height: 270px;
+    background: #fff;
+    border-radius: 5px;
+    position: relative;
+    h1 {
+      color: rgba(0, 0, 0, 0.5);
+      text-align: center;
+      margin: 10px 0;
+    }
+    .login_form {
+      width: 100%;
+      padding: 0 25px;
+      position: absolute;
+      bottom: 0;
+      .button {
+        display: flex;
+        justify-content: flex-end;
+      }
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import Vue from "vue";
+import "./plugins/axios";
 import App from "./App.vue";
 import router from "./router";
+import "./assets/fonts/iconfont.css";
+import "./assets/global.css";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import axios from "axios";
+
+Vue.use(ElementUI);
+
+Vue.prototype.$http = axios;
+axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/";
 
 Vue.config.productionTip = false;
 
